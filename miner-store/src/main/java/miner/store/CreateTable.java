28bf5bc82fa1471f9c2846fec98a0bbd7c016b00
kangpengming1107 <kpcmc5211314@gmail.com>
2015-08-13 16:37:30 +0800

    ImportDatat

--- miner-store/src/main/java/miner/store/CreateTable.java
+++ miner-store/src/main/java/miner/store/CreateTable.java
@@ -4,88 +4,82 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Calendar;
 import java.util.Properties;
 import java.sql.Connection;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 import com.mysql.jdbc.Driver;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.*;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
 
-public class CreatTable {
-	private static Configuration configuration = null;
-	static{
-		configuration = HBaseConfiguration.create();
-		configuration.set("hbase.zookeeper.quorum", "127.0.0.1");
-	  	configuration.set("hbase.rootdir","hdfs://master:8020/hbase");
-  	   configuration.set("hbase.master", "hdfs://master:60000");
-	}
-	public static void main(String args[]) throws SQLException{
-		
-		new CreatTable().mysqlCheck(configuration);
-	}
-	
-	public void mysqlCheck(Configuration conf) throws SQLException{
-		boolean flag = true;
-		String url = "jdbc:mysql://localhost/test?useUnicode=true&characterEncoding=utf8";
-		Properties property = new Properties();
-		property.put("user", "root");
-		property.put("password", "root");
-		Driver driver = new Driver();
-		Connection con = driver.connect(url, property);
-		Statement sta = con.createStatement();
-		String sql = "select * from dataid";
-		ResultSet rs = sta.executeQuery(sql);
-			rs.last(); 
-			String wid = rs.getString("wid");
-			String pid = rs.getString("pid");
-			String tid = rs.getString("tid");
-			String dataid = rs.getString("dataid");
-			String foreignkey = rs.getString("foreignkey");
-			if(foreignkey.equals("none")){
-				flag = false;
-			}
-			String tablename = wid+pid+tid+dataid;
-			createTable(conf, tablename, flag);
-	}
-	
-	
-	public void createTable(Configuration conf,String tableName,boolean flag){
-		HBaseAdmin admin;
-		  try {
-		   admin = new HBaseAdmin(conf);
-		   if(admin.tableExists(tableName)){
-		    System.err.print(tableName+"is exist and please check it");
-		   }else{
-		   HTableDescriptor tableDescriptor=new HTableDescriptor(TableName.valueOf(tableName));
-		   tableDescriptor.addFamily(new HColumnDescriptior("info"));
-		   tableDescriptor.addFamily(new HColumnDescriptor("description"));
-		   tableDescriptor.addFamily(new HColumnDescriptor("property"));
-		   tableDescriptor.addFamily(new HColumnDescriptor("link"));
-		   if(flag){
-		   tableDescriptor.addFamily(new HColumnDescriptor("forgeignkey"));
-		   tableDescriptor.addFamily(new HColumnDescriptor("foreignvalue"));
-		   }
-		   admin.createTable(tableDescriptor);
-		   System.out.println("end create table");
-		   }
-		  } catch (MasterNotRunningException e) {
-		   // TODO Auto-generated catch block
-		   e.printStackTrace();
-		  } catch (ZooKeeperConnectionException e) {
-		   // TODO Auto-generated catch block
-		   e.printStackTrace();
-		  } catch (IOException e) {
-		   // TODO Auto-generated catch block
-		   e.printStackTrace();
-		  }
-		 }
+public class CreateTable {
+    private static Configuration configuration = null;
+    static{
+        configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "127.0.0.1");
+        configuration.set("hbase.rootdir","hdfs://master:8020/hbase");
+        configuration.set("hbase.master", "hdfs://master:60000");
+    }
+    public static void main(String args[]) throws SQLException{
+
+        new CreateTable().mysqlCheck(configuration);
+    }
+
+    public void mysqlCheck(Configuration conf) throws SQLException{
+        boolean flag = true;
+        String url = "jdbc:mysql://localhost/test?useUnicode=true&characterEncoding=utf8";
+        Properties property = new Properties();
+        property.put("user", "root");
+        property.put("password", "root");
+        Driver driver = new Driver();
+        Connection con = driver.connect(url, property);
+        Statement sta = con.createStatement();
+        String sql = "select * from dataid";
+        ResultSet rs = sta.executeQuery(sql);
+        rs.last();
+        String wid = rs.getString("wid");
+        String pid = rs.getString("pid");
+        String tid = rs.getString("tid");
+        String dataid = rs.getString("dataid");
+        String foreignkey = rs.getString("foreignkey");
+        if(foreignkey.equals("none")){
+            flag = false;
+        }
+        String tablename = wid+pid+tid+dataid;
+        createTable(conf, tablename, flag);
+    }
+
+
+    public void createTable(Configuration conf,String tableName,boolean flag){
+        HBaseAdmin admin;
+        try {
+            admin = new HBaseAdmin(conf);
+            if(admin.tableExists(tableName)){
+                System.err.print(tableName+"is exist and please check it");
+            }else{
+                HTableDescriptor tableDescriptor=new HTableDescriptor(TableName.valueOf(tableName));
+                tableDescriptor.addFamily(new HColumnDescriptor("info"));
+                tableDescriptor.addFamily(new HColumnDescriptor("description"));
+                tableDescriptor.addFamily(new HColumnDescriptor("property"));
+                tableDescriptor.addFamily(new HColumnDescriptor("link"));
+                if(flag){
+                    tableDescriptor.addFamily(new HColumnDescriptor("forgeignkey"));
+                    tableDescriptor.addFamily(new HColumnDescriptor("foreignvalue"));
+                }
+                admin.createTable(tableDescriptor);
+                System.out.println("end create table");
+            }
+        } catch (MasterNotRunningException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ZooKeeperConnectionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
 
---
+++ miner-store/src/main/java/miner/store/ImportData.java
@@ -0,0 +1,130 @@
+package miner.store;
+
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by root on 15-8-12.
+ */
+public class ImportData {
+
+    public static  void main(String[] args){
+       String data = "{\n" +
+                "\"workstation_id\":\"1\",\n" +
+                "\"project_id\":\"1\",\n" +
+                "\"task_id\":\"1\",\n" +
+                "\"data_id\":\"1\",\n" +
+                "\"description\":\"city\",\n" +
+                "\"property\":{\n" +
+                "\t\"cityname\":\"Xian\",\n" +
+                "\t\"url\":[\"12\",\"34\"]\n" +
+                "},\n" +
+                "\"row_key\":\"12233\",\n" +
+                "\"foreign_key\":\"1233344\",\n" +
+                "\"foreign_value\":\"kp\",\n" +
+                "\"link\":\"alone\"\n" +
+                "}";
+        new ImportData().importData(data);
+    }
+
+    public void importData(String data){
+
+        try {
+            String rowKey = null;
+            JSONObject jsonObject = new JSONObject(data);
+            //获取info中的信息
+            String workStationID = jsonObject.getString("workstation_id");
+            String projectID = jsonObject.getString("project_id");
+            String taskID = jsonObject.getString("task_id");
+            String dataID = jsonObject.getString("data_id");
+            //构造出表名，以各个id命名
+            String tableName = workStationID+projectID+taskID+dataID;
+
+            String foreignKey = jsonObject.getString("foreign_key");
+            String foreignValue = jsonObject.getString("foreign_value");
+            String link = jsonObject.getString("link");
+            String rowKeyBak = jsonObject.getString("row_key");
+            String description = jsonObject.getString("description");
+
+            //构造出行键，当rowkey不为none的时候，使用时间戳与行键，否则使用UUID
+            if(!rowKeyBak.equals("none")) {
+                rowKey = System.currentTimeMillis() + "$" + rowKeyBak;
+            }else {
+               rowKey = UUID.randomUUID().toString();
+            }
+
+            //对property进行处理
+            String property = jsonObject.getString("property");
+            JSONObject propertyjson = new JSONObject(property);
+            Iterator keys = propertyjson.keys();
+            while (keys.hasNext()){
+                String key = keys.next().toString();
+                String propertyValue = propertyjson.getString(key);
+                Pattern pattern = Pattern.compile("\\[");
+                Matcher matcher = pattern.matcher(propertyValue);
+                if(matcher.find()){
+                JSONArray jsonArray = new JSONArray(propertyValue);
+                for (int i = 0; i < jsonArray.length();i++) {
+                    addData(null, tableName, rowKey, "property", key, jsonArray.getString(i));
+                }
+                }
+            }
+
+            addData(null,tableName,rowKey,"info","workStationID",workStationID);
+            addData(null,tableName,rowKey,"info","projectID",projectID);
+            addData(null,tableName,rowKey,"info","taskID",taskID);
+            addData(null,tableName,rowKey,"info","dataID",dataID);
+            addData(null,tableName,rowKey,"link","link",link);
+            addData(null,tableName,rowKey,"description","description",description);
+            if(!foreignKey.equals("alone")){
+                addData(null,tableName,rowKey,"foreignKey","foreignKey",foreignKey);
+                addData(null,tableName,rowKey,"foreignValue","foreignValue",foreignValue);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void addData(Configuration configuration,String tableName,String Rowkey,String family,String column,
+                        String value){
+        HBaseAdmin admin;
+        try {
+            admin = new HBaseAdmin(configuration);
+            if(admin.tableExists(tableName)){
+                @SuppressWarnings("resource")
+                HTable table=new HTable(configuration, tableName);
+                Put put=new Put(Bytes.toBytes(Rowkey));
+                put.add(Bytes.toBytes(family), Bytes.toBytes(column), Bytes.toBytes(value));
+                table.put(put);
+                //System.out.println("add success!");
+            }else{
+                System.out.println(tableName+"Table does not exist!");
+            }
+        } catch (MasterNotRunningException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ZooKeeperConnectionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
