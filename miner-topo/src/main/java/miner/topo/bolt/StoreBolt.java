3ed95236e1518b3677616d394125c778c199fcb1
cutoutsy <cutoutsy@gmail.com>
2016-06-08 17:36:57 +0800

    存入数据库的时候进行数据重复性检测

--- miner-topo/src/main/java/miner/topo/bolt/StoreBolt.java
+++ miner-topo/src/main/java/miner/topo/bolt/StoreBolt.java
@@ -12,12 +12,16 @@ import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
 import miner.store.ImportData;
 import miner.utils.MySysLogger;
+import miner.utils.RedisUtil;
+import redis.clients.jedis.Jedis;
 
 import java.util.Map;
 
 public class StoreBolt extends BaseRichBolt {
 
 	private static MySysLogger logger = new MySysLogger(StoreBolt.class);
+	private Jedis jedis;
+	private RedisUtil ru;
 	private OutputCollector _collector;
 
 	public void execute(Tuple tuple) {
@@ -25,11 +29,18 @@ public class StoreBolt extends BaseRichBolt {
         String globalInfo  = tuple.getString(0);
         String data = tuple.getString(1);
 		try {
-			ImportData.importData(data);
 
-			logger.info(globalInfo+":save into hbase succeed!");
-//			logger.info(data); //集群上注释
-			_collector.ack(tuple);
+            String workspace_id = get_workspace_id(globalInfo);
+            //利用redis来进行数据的去重
+            if(!jedis.sismember(workspace_id+"_unique", globalInfo)) {
+                //将数据存放进HBase
+                ImportData.importData(data);
+                logger.info(globalInfo + ":save into hbase succeed!");
+                jedis.sadd(workspace_id+"_unique", globalInfo);
+                _collector.ack(tuple);
+            }else{
+                logger.warn(globalInfo+":已经存进数据库了.");
+            }
 		} catch (Exception ex) {
 			_collector.fail(tuple);
 			logger.error("store error!"+MySysLogger.formatException(ex));
@@ -42,8 +53,15 @@ public class StoreBolt extends BaseRichBolt {
 
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
 		this._collector = collector;
+        ru = new RedisUtil();
+        jedis = ru.getJedisInstance();
 	}
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 	}
+
+    //获取workspace的id
+    private String get_workspace_id(String global_info){
+        return global_info.split("-")[0];
+    }
 }
--- miner-topo/src/test/java/miner/bolt/StoreBoltTest.java
+++ miner-topo/src/test/java/miner/bolt/StoreBoltTest.java
@@ -11,6 +11,8 @@ import backtype.storm.tuple.Tuple;
 import miner.spider.utils.IOUtil;
 import miner.store.ImportData;
 import miner.utils.MySysLogger;
+import miner.utils.RedisUtil;
+import redis.clients.jedis.Jedis;
 
 import java.util.Map;
 
@@ -18,17 +20,29 @@ public class StoreBoltTest extends BaseRichBolt {
 
 	private static MySysLogger logger = new MySysLogger(StoreBoltTest.class);
 	private OutputCollector _collector;
+    private Jedis jedis;
+    private RedisUtil ru;
 
 	public void execute(Tuple tuple) {
 		try {
 			String globalInfo  = tuple.getString(0);
 			String data = tuple.getString(1);
 
+			System.err.println(globalInfo + "=========");
 //			ImportData.importData(data);
 
 			System.err.println(data);
-			IOUtil.writeFile("/Users/cutoutsy/jd.txt", data+"\n", true, "utf-8");
-			logger.info(globalInfo+":save into hbase succeed!");
+            String workspace_id = get_workspace_id(globalInfo);
+
+
+            if(!jedis.sismember(workspace_id+"_unique", globalInfo)) {
+
+                IOUtil.writeFile("/Users/cutoutsy/jd.txt", data + "\n", true, "utf-8");
+                logger.info(globalInfo + ":save into hbase succeed!");
+                jedis.sadd(workspace_id+"_unique", globalInfo);
+            }else{
+                System.out.println(globalInfo+":已经存进数据库了.");
+            }
 //			logger.info(data); //集群上注释
 			_collector.ack(tuple);
 		} catch (Exception ex) {
@@ -40,7 +54,13 @@ public class StoreBoltTest extends BaseRichBolt {
 
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
 		this._collector = collector;
+        ru = new RedisUtil();
+        jedis = ru.getJedisInstance();
 	}
+
+    private String get_workspace_id(String global_info){
+        return global_info.split("-")[0];
+    }
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
 	}
